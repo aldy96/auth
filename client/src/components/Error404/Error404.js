7118ee7a406e7bbd4c76b958740f5be9b467669e
Igor Dudek <dudekigor@gmail.com>
2020-12-18 11:23:35 +0100

    Completed Error404 component

--- client/src/components/Error404/Error404.js
+++ client/src/components/Error404/Error404.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { StyledWrapper, StyledButton } from './Error404.styled';
+import {
+  StyledWrapper,
+  StyledHeader,
+  StyledButton,
+  StyledLoader,
+} from './Error404.styled';
 
 const Error404 = () => {
   const history = useHistory();
 
   return (
     <StyledWrapper>
+      <StyledHeader>Something Went Wrong</StyledHeader>
+      <StyledLoader type='Oval' color='#0074d9' height={80} width={80} />
       <StyledButton primary type='button' onClick={() => history.goBack()}>
         Go Back
       </StyledButton>
--- client/src/components/Error404/Error404.styled.js
+++ client/src/components/Error404/Error404.styled.js
@@ -1,12 +1,27 @@
 import styled, { css } from 'styled-components';
+import Loader from 'react-loader-spinner';
 
 export const StyledWrapper = styled.main`
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: space-evenly;
 `;
+export const StyledHeader = styled.h1`
+  text-align: center;
+  margin: 16px 0;
+  padding: 0 16px;
+`;
+export const StyledLoader = styled(Loader)`
+  flex-basis: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 16px 0px;
+`;
+
 export const StyledButton = styled.button`
-  margin: 8px 16px;
+  margin: 16px 16px;
   padding: 4px 24px;
   background: transparent;
   border: 2px solid #0074d9;
@@ -23,7 +38,7 @@ export const StyledButton = styled.button`
       transition: all ease-in-out 300ms;
       &:hover {
         box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.1);
-        transform: translateY(3px);
+        transform: translateY(2px);
       }
     `};
 `;
