3a0b6aa3e4039fe1a3e655743f59b1506bb91055
Igor Dudek <dudekigor@gmail.com>
2020-12-28 22:32:50 +0100

    Update Menu

--- client/src/components/nav/menu/Menu.js
+++ client/src/components/nav/menu/Menu.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import ThemeToggler from '../themeToggler/ThemeToggler';
+import ThemeToggler from '../themetoggler/ThemeToggler';
 // Style
 import {
   StyledStaticMenu,
